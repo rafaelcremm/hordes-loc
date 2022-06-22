eaf98a44a4c59cf5e04a0fdcefabbc9699eed091
rafaelcremm <84933883+rafaelcremm@users.noreply.github.com>
2022-06-22 13:18:05 -0300

    Update war.js

--- loc/ui/war.js
+++ loc/ui/war.js
@@ -2,32 +2,38 @@ export default {
   status: {
     en: 'War Status',
     fr: 'Statut de la Guerre',
+    pt: 'Status da Guerra',
     tr: 'Savaş durumu'
   },
 
   duration: {
     en: 'Duration',
     fr: 'Durée',
+    pt: 'Duração',
     tr: 'Süre'
   },
 
   player: {
     en: 'Player',
     fr: 'Joueur',
+    pt: 'Jogador',
     tr: 'Oyuncu'
   },
 
   statustypes: [{
     en: 'Ended',
     fr: 'Terminé',
+    pt: 'Finalizado',
     tr: 'Sona erdi'
   }, {
     en: 'Ending Soon',
-    fr: 'Bientôt terminé',
+    fr: 'Bientôt terminé',    
+    pt: 'Termina em breve',
     tr: 'Yakında sona erecek'
   }, {
     en: 'Active',
     fr: 'Actif',
+    pt: 'Ativo',
     tr: 'Aktif'
   }]
 }
